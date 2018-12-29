a2fd04366ffb8052aa14723d82eeafda53eb5ff2
boapps <botosandris2000@gmail.com>
2018-12-29 10:59:36 +0100

    infó képernyő javítása/fejlesztése

--- lib/screens/aboutScreen.dart
+++ lib/screens/aboutScreen.dart
@@ -14,9 +14,6 @@ class AboutScreen extends StatefulWidget {
   AboutScreenState createState() => new AboutScreenState();
 }
 
-
-
-
 class AboutScreenState extends State<AboutScreen> {
 
   @override
@@ -32,214 +29,144 @@ class AboutScreenState extends State<AboutScreen> {
           title: new Text("e-Szivacs 2"),
           actions: <Widget>[
           ],
-          /* bottom: new PreferredSize(
-              child: new LinearProgressIndicator(
-                value: 0.7,
-              ),
-              preferredSize: null),*/
         ),
-        body: new Stack(
-          children: <Widget>[
-            new ConstrainedBox(
-                constraints: const BoxConstraints.expand(),
-                child: Image.asset("assets/noise.webp",fit: BoxFit.fill,),
-            ),
-
-            new Container(
-              child: new Center(
-                child: new Container(
-                  color: Colors.white12,
-                  child: new BackdropFilter(
-                    filter: new ImageFilter.blur(sigmaX: 3.0, sigmaY: 3.0),
-                    child: new Container(
-                      decoration: new BoxDecoration(
-                          color: Colors.white30.withOpacity(0.5)
+        body:
+            new Center(
+              child: Container(
+                child: new ListView(
+                  padding: EdgeInsets.all(20),
+                  children: <Widget>[
+                    new Container(
+                      child: new Text(
+                        'e-Szivacs',
+                        style: new TextStyle(fontSize: 28.0,),
+                        textAlign: TextAlign.center,
                       ),
-                      child: new Center(
-                        
-                        child: new SingleChildScrollView(
-                        padding: EdgeInsets.all(40.0),
-                          child: new Container(
-                          child: new Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: <Widget>[
-                              new Container(
-                                child: new Text(
-                                  'e-Szivacs',
-                                  style: new TextStyle(fontSize: 22.0, color: Colors.black87),
-                                ),
-                                padding: EdgeInsets.only(bottom: 30.0),
-                              ),
-                              new Row(
-                                children: <Widget>[
-                                  new Text(
-                                    'verzió: ',
-                                    style: new TextStyle(fontSize: 22.0, color: Colors.black87),
-                                  ),
-                                  new Text(' 2.0',
-                                    style: new TextStyle(fontSize: 22.0, color: Colors.blue),
-                                  ),
-                                ],
-                                mainAxisAlignment: MainAxisAlignment.center,
-                              ),
-                              Container(
-                              child: new Row(
-                                children: <Widget>[
-                                  new Text(
-                                    'készítette: ',
-                                    style: new TextStyle(fontSize: 22.0, color: Colors.black87),
-                                  ),
-                                  new Text(' BoA',
-                                    style: new TextStyle(fontSize: 22.0, color: Colors.blue),
-                                  ),
-                                ],
-                                mainAxisAlignment: MainAxisAlignment.center,
-                              ),
-                                color: Colors.white,
-                              ),
-                              new Row(
-                                children: <Widget>[
-                                  new Text(
-                                    'made with: ',
-//                                    style: Theme.of(context).textTheme.title,
-                                    style: new TextStyle(fontSize: 22.0, color: Colors.black87),
-                                  ),
-                                  new Text(' Flutter',
-                                    style: new TextStyle(fontSize: 22.0, color: Colors.blue),
-                                  ),
-                                ],
-                                mainAxisAlignment: MainAxisAlignment.center,
-                              ),
-//                              new Container(
-//                                padding: EdgeInsets.only(top: 20.0),
-//                                child: new Row(
-//                                  children: <Widget>[
-//                                    new Icon(Icons.live_tv, color: Colors.red,),
-//                                    new FlatButton(onPressed: emailPressed, child: new Text("YouTube", style: new TextStyle(color: Colors.red, fontSize: 20.0,),)),
-//                                  ],
-//                                  crossAxisAlignment: CrossAxisAlignment.center,
-//                                  mainAxisAlignment: MainAxisAlignment.center,
-//                                ),
-//                                alignment: new Alignment(0.0, 0.0),
-//                              ),
-//                              new Container(
-//                                child: new Row(
-//                                  children: <Widget>[
-//                                    new Icon(Icons.email, color: Colors.green,),
-//                                    new FlatButton(onPressed: emailPressed, child: new Text("e-szivacs@gmail.com", style: new TextStyle(color: Colors.green, fontSize: 20.0,),)),
-//                                  ],
-//                                  crossAxisAlignment: CrossAxisAlignment.center,
-//                                  mainAxisAlignment: MainAxisAlignment.center,
-//                                ),
-//                              ),
-                              new FlatButton(onPressed: _launchYTURL,
-                                        child: new Row(
-                                           children: <Widget>[
-                                             new Container(
-//                                               child: new Icon(Icons.message, color: Colors.blue, size: 20.0,),
-                                               child: new Icon(IconData(0xf5c3, fontFamily: "Material Design Icons"), color: Colors.red, size: 20.0,),
-                                               padding: EdgeInsets.all(5.0),
-                                             ),
-                                             new Text(
-                                               "YouTube", style: new TextStyle(color: Colors.red, fontSize: 20.0,),
-                                             ),
-                                           ],
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                        )
-                                    ),
-                              new FlatButton(onPressed: _launchTelURL,
-                                  child: new Row(
-                                    children: <Widget>[
-                                      new Container(
-//                                               child: new Icon(Icons.message, color: Colors.blue, size: 20.0,),
-                                        child: new Icon(IconData(0xf501, fontFamily: "Material Design Icons"), color: Colors.blue, size: 20.0,),
-                                        padding: EdgeInsets.all(5.0),
-                                      ),
-                                      new Text(
-                                        "Telegram", style: new TextStyle(color: Colors.blue, fontSize: 20.0,),
-                                      ),
-                                    ],
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                  )
-                              ),
-                              new FlatButton(onPressed: _launchGmailURL,
-                                        child: new Row(
-                                           children: <Widget>[
-                                             new Container(
-//                                               child: new Icon(Icons.message, color: Colors.blue, size: 20.0,),
-                                               child: new Icon(IconData(0xf2ab, fontFamily: "Material Design Icons"), color: Colors.red, size: 20.0,),
-                                               padding: EdgeInsets.all(5.0),
-                                             ),
-                                             new Text(
-                                               "eSzivacs@gmail.com",
-                                               style: new TextStyle(
-                                                 color: Colors.red,
-                                                 fontSize: 20.0,),
-                                             ),
-                                           ],
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                        )
-                                    ),
-                              new FlatButton(onPressed: _launchGithubURL,
-                                        child: new Row(
-                                           children: <Widget>[
-                                             new Container(
-//                                               child: new Icon(Icons.message, color: Colors.blue, size: 20.0,),
-                                               child: new Icon(IconData(0xf2a4, fontFamily: "Material Design Icons"), color: Colors.black, size: 20.0,),
-                                               padding: EdgeInsets.all(5.0),
-                                             ),
-                                             new Text(
-                                               "GitHub", style: new TextStyle(color: Colors.black, fontSize: 20.0,),
-                                             ),
-                                           ],
-                                          mainAxisAlignment: MainAxisAlignment.center,
-                                        )
-                                    ),
-                              new FlatButton(onPressed: _launchIGURL,
-                                  child: new Row(
-                                    children: <Widget>[
-                                      new Container(
-//                                               child: new Icon(Icons.message, color: Colors.blue, size: 20.0,),
-                                        child: new Icon(IconData(0xf2fe,
-                                            fontFamily: "Material Design Icons"),
-                                          color: Colors.pink, size: 20.0,),
-                                        padding: EdgeInsets.all(5.0),
-                                      ),
-                                      new Text(
-                                        "Instagram", style: new TextStyle(
-                                        color: Colors.pink, fontSize: 20.0,),
-                                      ),
-                                    ],
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                  )
-                              ),
-//                                alignment: Alignment(0.0, 0.0),
-
-                            ],
-//                            crossAxisAlignment: CrossAxisAlignment.center,
+                      padding: EdgeInsets.only(bottom: 30.0),
+                    ),
+                    new Row(
+                      children: <Widget>[
+                        new Text(
+                          'verzió: ',
+                          style: new TextStyle(fontSize: 22.0, ),
                         ),
-                            padding: EdgeInsets.all(10.0),
-                            decoration: BoxDecoration(borderRadius: BorderRadius.circular(10.0),color: Colors.white, boxShadow: [
-                              new BoxShadow(
-                                color: Colors.black26,
-                                blurRadius: 25.0,
-                              ),
-                            ]),
-
+                        new Text(' 2.0',
+                          style: new TextStyle(fontSize: 22.0, color: Colors.blue),
+                        ),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
+                    ),
+                    Container(
+                      child: new Row(
+                        children: <Widget>[
+                          new Text(
+                            'készítette: ',
+                            style: new TextStyle(fontSize: 22.0, ),
                           ),
+                          new Text(' BoA',
+                            style: new TextStyle(fontSize: 22.0, color: Colors.blue),
+                          ),
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.center,
                       ),
                     ),
+                    new Row(
+                      children: <Widget>[
+                        new Text(
+                          'made with: ',
+//                                    style: Theme.of(context).textTheme.title,
+                          style: new TextStyle(fontSize: 22.0, ),
+                        ),
+                        new Text(' Flutter',
+                          style: new TextStyle(fontSize: 22.0, color: Colors.blue),
+                        ),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.center,
                     ),
-                  ),
-                  ),
+                    new FlatButton(onPressed: _launchYTURL,
+                        child: new Row(
+                          children: <Widget>[
+                            new Container(
+                              child: new Icon(IconData(0xf5c3, fontFamily: "Material Design Icons"), color: Colors.red, size: 20.0,),
+                              padding: EdgeInsets.all(5.0),
+                            ),
+                            new Text(
+                              "YouTube", style: new TextStyle(color: Colors.red, fontSize: 20.0,),
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        ),
+                    ),
+                    new FlatButton(onPressed: _launchTelURL,
+                        child: new Row(
+                          children: <Widget>[
+                            new Container(
+                              child: new Icon(IconData(0xf501, fontFamily: "Material Design Icons"), color: Colors.blue, size: 20.0,),
+                              padding: EdgeInsets.all(5.0),
+                            ),
+                            new Text(
+                              "Telegram", style: new TextStyle(color: Colors.blue, fontSize: 20.0,),
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        )
+                    ),
+                    new FlatButton(onPressed: _launchGmailURL,
+                        child: new Row(
+                          children: <Widget>[
+                            new Container(
+                              child: new Icon(IconData(0xf2ab, fontFamily: "Material Design Icons"), color: Colors.red, size: 20.0,),
+                              padding: EdgeInsets.all(5.0),
+                            ),
+                            new Text(
+                              "eSzivacs@gmail.com",
+                              style: new TextStyle(
+                                color: Colors.red,
+                                fontSize: 20.0,),
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        )
+                    ),
+                    new FlatButton(onPressed: _launchGithubURL,
+                        child: new Row(
+                          children: <Widget>[
+                            new Container(
+                              child: new Icon(IconData(0xf2a4, fontFamily: "Material Design Icons"), color: Colors.black, size: 20.0,),
+                              padding: EdgeInsets.all(5.0),
+                            ),
+                            new Text(
+                              "GitHub", style: new TextStyle(fontSize: 20.0,),
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        )
+                    ),
+                    new FlatButton(onPressed: _launchIGURL,
+                        child: new Row(
+                          children: <Widget>[
+                            new Container(
+                              child: new Icon(IconData(0xf2fe,
+                                  fontFamily: "Material Design Icons"),
+                                color: Colors.pink, size: 20.0,),
+                              padding: EdgeInsets.all(5.0),
+                            ),
+                            new Text(
+                              "Instagram", style: new TextStyle(
+                              color: Colors.pink, fontSize: 20.0,),
+                            ),
+                          ],
+                          mainAxisAlignment: MainAxisAlignment.center,
+                        )
+                    ),
+                  ],
+                ),
               ),
-              ),
-    ]
-        )
-
+            ),
         )
     );
   }
+
   _launchYTURL() async {
     const url = 'https://www.youtube.com/channel/UC1V9Sdq4RlYjzZEkB9bzwrA';
     if (await canLaunch(url)) {
