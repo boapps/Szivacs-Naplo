7dee22eab9c781f96404966e0e1c15a74b214e55
boapps <botosandris2000@gmail.com>
2019-02-03 11:47:34 +0100

    tervek a jövőre

---
+++ lib/Datas/Account.dart
@@ -0,0 +1,11 @@
+import 'User.dart';
+import 'Evaluation.dart';
+import 'Absence.dart';
+
+class Account {
+//todo Csinálni egy olyan rendszert, amiben a lekérdezéseket Account.refresh()-el lehetne
+//todo megoldani és az Account alatt lenne minden: User user, String studentString,
+//todo DateTime lastrefresh, List<Evaluation> evaluations stb...
+//todo Így könnyebb lenne különválasztani a felhasználókat és optimálisabban
+//todo lehetne kezelni a lekérdezéseket, offline adatokat.
+}
