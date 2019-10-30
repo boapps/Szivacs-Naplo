f8293d65edce16c2f99f68abdf41e4eeb69ed015
boapps <17887448+boapps@users.noreply.github.com>
2019-10-30 07:52:04 +0100

    build-script javítása

--- build-apks.sh
+++ build-apks.sh
@@ -1,33 +1,35 @@
+#!/usr/bin/env bash
+
 version=$1
 fossness="foss"
-flavor="${fossness}_release"
 
 if [[ -z "$version" ]]; then
-	echo "Nem adtál meg verziót!"
-	echo "pl.: sh build-apks.sh 1.2.3"
+    echo "Nem adtál meg verziót!"
+    echo "pl.: sh build-apks.sh 1.2.3"
 else
-	echo "Fájlok építése a $version verzióhoz..."
+    echo "Fájlok építése a $version verzióhoz..."
 
-	if [[ $2 == "beta" ]]; then
-		flavor="${fossness}_beta"
-		version="$version-beta"
-		echo "building beta version"
-		flutter build apk --flavor play_beta
-		flutter build appbundle --flavor play_beta --target-platform android-arm,android-arm64
-		flutter build apk --flavor play_beta --target-platform android-arm,android-arm64 --split-per-abi
-	else
-		echo "building release version"
-		flutter build apk --flavor play_release
-		flutter build appbundle --flavor play_release --target-platform android-arm,android-arm64
-		flutter build apk --flavor play_release --target-platform android-arm,android-arm64 --split-per-abi
-	fi
+    if [[ $2 == "beta" ]]; then
+        flavor="${fossness}_beta"
+        version="$version-beta"
+        echo "building beta version"
+        flutter build apk --flavor $flavor
+        flutter build appbundle --flavor $flavor --target-platform android-arm,android-arm64
+        flutter build apk --flavor $flavor --target-platform android-arm,android-arm64 --split-per-abi
+    else
+       flavor="${fossness}_release"
+       echo "building release version"
+       flutter build apk --flavor $flavor
+       flutter build appbundle --flavor $flavor --target-platform android-arm,android-arm64
+       flutter build apk --flavor $flavor --target-platform android-arm,android-arm64 --split-per-abi
+    fi
 
-	echo "A fájlok készen vannak, átmásolás a közös mappába..."
+    echo "A fájlok készen vannak, átmásolás a közös mappába..."
 
-	cp build/app/outputs/apk/$flavor/release/app-$flavor-release.apk ~/release_apks/e-szivacs_v$version.apk
-	cp build/app/outputs/bundle/${flavor}Release/app.aab ~/release_apks/e-szivacs_v$version.aab
-	cp build/app/outputs/apk/$flavor/release/app-$flavor-armeabi-v7a-release.apk ~/release_apks/e-szivacs_v$version-arm.apk
-	cp build/app/outputs/apk/$flavor/release/app-$flavor-arm64-v8a-release.apk ~/release_apks/e-szivacs_v$version-arm64.apk
+    cp build/app/outputs/apk/$flavor/release/app-$flavor-release.apk ~/release_apks/e-szivacs_${fossness}_v$version.apk
+    cp build/app/outputs/bundle/${flavor}Release/app.aab ~/release_apks/e-szivacs_${fossness}_v$version.aab
+    cp build/app/outputs/apk/$flavor/release/app-$flavor-armeabi-v7a-release.apk ~/release_apks/e-szivacs_${fossness}_v$version-arm.apk
+    cp build/app/outputs/apk/$flavor/release/app-$flavor-arm64-v8a-release.apk ~/release_apks/e-szivacs_${fossness}_v$version-arm64.apk
 
-	echo "Kész!"
+    echo "Kész!"
 fi
