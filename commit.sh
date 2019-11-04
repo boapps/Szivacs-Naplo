fdd0dc4659554dd5737daaeffc4b8fc0d4410108
boapps <17887448+boapps@users.noreply.github.com>
2019-11-04 22:55:50 +0100

    commit script

---
+++ commit.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+m=$1
+
+if [[ -z '$m' ]]; then
+    echo 'no commit message given'
+else
+    flutter pub get
+
+    git commit -a -m $m
+    echo 'rsync'
+    rsync -av --exclude='.git*' --exclude='README.md' ./ ../.szivacs-tmp/
+    echo 'rsyncend'
+
+    git checkout master
+
+    echo 'rsync'
+    rsync -av --exclude='.git*' --exclude='README.md' ../.szivacs-tmp/ ./
+    echo 'rsyncend'
+    sh to_foss.sh
+    flutter pub get
+    git commit -a -m $m
+
+    git checkout play-edition
+fi
