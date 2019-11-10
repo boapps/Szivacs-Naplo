dc89c6176097d206f8c3d9bf78bd1613e1a8fc4a
boapps <17887448+boapps@users.noreply.github.com>
2019-11-10 19:06:50 +0100

    következő óra valós időben frissítése

--- lib/screens/mainScreen.dart
+++ lib/screens/mainScreen.dart
@@ -140,6 +140,7 @@ class MainScreenState extends State<MainScreen> {
       globals.firstMain = false;
     }
     startDate = now;
+    new Timer.periodic(Duration(seconds: 10), (Timer t) => setState((){mainScreenCards = feedItems();}));
   }
 
   List<Widget> feedItems() {
@@ -170,8 +171,9 @@ class MainScreenState extends State<MainScreen> {
       }
     }
 
-    if (realLessons.length > 0 && isLessonsToday)
+    if (realLessons.length > 0 && isLessonsToday) {
       feedCards.add(new LessonCard(realLessons, context, now));
+    }
     try {
       feedCards.sort((Widget a, Widget b) {
         return b.key.toString().compareTo(a.key.toString());
