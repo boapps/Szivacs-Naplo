dffd6f13c583d1a773cbdbf776ebbae929073918
UnknownPlayer78 <nyitraiszabolcs4@gmail.com>
2020-01-26 15:48:08 +0100

    r

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/home/unknown/Downloads/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/unknown/projects/szivacs_naplo/Szivacs-Naplo-RedyAu"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=C:\Users\fodor\Documents\_Android\FlutterSDK\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\fodor\Documents\_Android\Flutter\VSCODE\Szivacs_c211b7c_1\Szivacs-Naplo"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/home/unknown/Downloads/flutter/bin/cache/artifacts/engine/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=1"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=C:\Users\fodor\Documents\_Android\FlutterSDK\flutter\bin\cache\artifacts\engine\ios"
